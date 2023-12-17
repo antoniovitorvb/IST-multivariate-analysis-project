10193ae302372893b6bc824fee463c34d304df1c
antoniovitorvb <vboasvitor@gmail.com>
2023-12-17 16:22:41 +0000

    1st review of report

--- project/multiAnalysis.R
+++ project/multiAnalysis.R
@@ -1,5 +1,6 @@
 library(readr)
 library(dplyr)
+library(tidyr)
 library(stringr)
 
 fitdata <- read_csv("fitness_class_2212.csv")
@@ -7,6 +8,8 @@ fitdata <- read_csv("fitness_class_2212.csv")
 # DATA EXAMINATION
 str(fitdata)
 summary(fitdata)
+
+sapply(fitdata, function(x) sum(is.na(x)))
 
 # TIDYING THE DATA
 
@@ -22,4 +25,29 @@ fitdata$day_of_week <- fitdata$day_of_week %>%
      substr(1, 3) %>% # gets only the 3 first elements of each string
      factor() # Converts to factor to be used as categorical variable
 
-cc
+unique(fitdata$time)
+fitdata$time <- ifelse(fitdata$time == "AM", 0, 1) %>%
+     factor()
+
+table(fitdata$category)
+
+fitdata <- fitdata %>%
+     mutate(category = na_if(category, "-")) #%>% # Replace "-" with NA
+     # drop_na(category) # Remove the rows in fitdata$category that is NA
+fitdata$category <- factor(fitdata$category)
+
+sapply(fitdata, function(x) sum(is.na(x)))
+
+get_na <- function(df) {
+     na_rows <- lapply(df, function(x) which(is.na(x))) %>% # get indices of NA values in each column
+          unlist() %>%
+          unique() # get the unique indices of all columns
+     return(df[na_rows, , drop = FALSE])
+}
+
+fit_na <- get_na(fitdata)
+
+# PRELIMINARY ANALYSIS
+
+# UNSUPERVISED LEARNING - K-MEANS
+
