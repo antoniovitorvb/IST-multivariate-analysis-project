a24abb0f934c1758998a304be6f41be70dde1a19
Darlan Paz de Jesus Santos Nascimento <48285227+StdioDesign@users.noreply.github.com>
2023-12-18 22:38:16 +0000

    Change matrix

--- project/regressao_project.R
+++ project/regressao_project.R
@@ -2,6 +2,14 @@
 library(caret)
 library(dplyr)
 library(vcd)
+library(ggplot2)
+library(reshape2)
+library(MLmetrics)
+library(grid)
+
+
+install.packages("grid")
+install.packages("MLmetrics")
 
 # Step 1: Load the dataset
 path = "C:/Users/Darla/Downloads/Acadêmico/Técnico Lisboa/Disciplinas/Análise Multivariada/IST-multivariate-analysis-project/project/fitness_class_2212.csv"
@@ -37,3 +45,53 @@ predictions <- ifelse(predictions > 0.5, 1, 0)
 conf_matrix <- confusionMatrix(as.factor(predictions), test_data$attended)
 fourfoldplot(conf_matrix$table, color = c("#CC6666", "#9999CC"), conf.level = 0, margin = 1)
 
+rain_conf_matrix <- data.frame(
+  Reference = factor(c("0", "0", "1", "1"), levels = c("0", "1")),
+  Prediction = factor(c("0", "1", "0", "1"), levels = c("0", "1")),
+  Count = c(748, 68,166, 192)
+)
+
+test_conf_matrix <- data.frame(
+  Reference = factor(c("0", "0", "1", "1"), levels = c("0", "1")),
+  Prediction = factor(c("0", "1", "0", "1"), levels = c("0", "1")),
+  Count = c(192, 48 ,17, 42)
+)
+
+# Create the train confusion matrix plot
+train_plot <- ggplot(train_conf_matrix, aes(x = Reference, y = Prediction, fill = Count)) +
+  geom_tile() +
+  geom_text(aes(label = Count), color = "black") +
+  scale_fill_gradient(low = "#FAEBDD", high = "#94A5D6") +
+  labs(title = "Train Confusion Matrix",
+       x = "Reference",
+       y = "Prediction") +
+  theme(plot.title = element_text(hjust = 0.5),
+        aspect.ratio = 1)
+
+# Create the test confusion matrix plot
+test_plot <- ggplot(test_conf_matrix, aes(x = Reference, y = Prediction, fill = Count)) +
+  geom_tile() +
+  geom_text(aes(label = Count), color = "black") +
+  scale_fill_gradient(low = "#FAEBDD", high = "#94A5D6") +
+  labs(title = "Test Confusion Matrix",
+       x = "Reference",
+       y = "Prediction") +
+  theme(plot.title = element_text(hjust = 0.5),
+        aspect.ratio = 1)
+
+# Arrange the plots side by side
+grid.arrange(train_plot, test_plot, nrow=1)
+
+train_summary <- confusionMatrix(train_pred, y_train)
+train_summary
+
+#F-1 Score
+train_f1_score <- F1_Score(as.factor(predictions), test_data$attended)
+cat("Train F1-Score:", train_f1_score, "\n")
+
+test_summary <- confusionMatrix(test_pred, y_test)
+test_summary
+
+#F-1 Score
+test_f1_score <- F1_Score(test_pred,y_test)
+cat("Test F1-Score:", test_f1_score, "\n")
